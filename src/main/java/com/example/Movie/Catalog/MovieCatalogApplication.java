6094159f0be69759f35df56f70909ecae93b3364
d2r3v <drv792@gmail.com>
2023-08-24 15:28:53 -0700

    Fixed Configurations

--- src/main/java/com/example/Movie/Catalog/MovieCatalogApplication.java
+++ src/main/java/com/example/Movie/Catalog/MovieCatalogApplication.java
@@ -2,12 +2,21 @@ package com.example.Movie.Catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieCatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogApplication.class, args);
 	}
 
+	@GetMapping
+	public String apiRoot() {
+		return "Hello world";
+	}
+
+	// spring.data.mongodb.uri = mongodb+srv://drv792:%23Googleabc12@cluster0.qfdr7ak.mongodb.net/
 }
