c15397b2d4e32132e541830615f2d2de0c0ca404
d2r3v <drv792@gmail.com>
2023-08-23 17:43:27 -0700

    Initial commit

---
+++ src/test/java/com/example/Movie/Catalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie.Catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
